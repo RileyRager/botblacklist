d5abf85f38f707376d5f065a5106820ebd799349
coderTSATISD <coderager9999@gmail.com>
2023-08-01 06:09:12 -0500

    fixed

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ tree = app_commands.CommandTree(bot)
 async def on_ready(): 
     print(f'loged in to {bot.user.name}')
     try:
-        tree.sync()
+        await tree.sync()
         print("synced to discord")
     except Exception as e: 
         print({e})
